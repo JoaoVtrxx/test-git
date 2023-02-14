2f7b67363fdaf4cd44232f745a6dac3a1cfd99b7
JoaoVtrxx <jdalpicio@outlook.com>
2023-02-14 11:24:31 -0300

    "ola"

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
-
-import 'myapp.dart';
+import 'package:flutter_1/home.dart';
 
 void main() {
   runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Gym App',
+      home: HomeScreen(),
+    );
+  }
 }
